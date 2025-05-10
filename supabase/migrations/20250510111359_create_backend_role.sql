28b6059769a859dac176a8928226040ede2be2d3
paoloechoes <anzanipaolo.enquires@gmail.com>
2025-05-10 15:29:01 +0200

    fix:  Grant service_role permissions to backend role

--- supabase/migrations/20250510111359_create_backend_role.sql
+++ supabase/migrations/20250510111359_create_backend_role.sql
@@ -2,7 +2,7 @@
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'backend') THEN
-        CREATE ROLE backend WITH LOGIN PASSWORD '{{BACKEND_ROLE_PASSWORD}}';
+        CREATE ROLE backend WITH LOGIN PASSWORD 'b11ba2a02b3065300d3c5963529d7347';
         -- Grant service_role permissions to backend
         GRANT service_role TO backend;
     END IF;
